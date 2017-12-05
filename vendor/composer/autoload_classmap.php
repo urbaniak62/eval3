17ae897d221a3d2401d33bcd4a27536abc0f2a14
urbaniak <urbaniaknicolas62@gmail.com>
2017-12-05 08:20:09 +0100

    resolve responsive probleme

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -12,6 +12,7 @@ return array(
     'App\\Http\\Controllers\\Auth\\LoginController' => $baseDir . '/app/Http/Controllers/Auth/LoginController.php',
     'App\\Http\\Controllers\\Auth\\RegisterController' => $baseDir . '/app/Http/Controllers/Auth/RegisterController.php',
     'App\\Http\\Controllers\\Auth\\ResetPasswordController' => $baseDir . '/app/Http/Controllers/Auth/ResetPasswordController.php',
+    'App\\Http\\Controllers\\ClientsController' => $baseDir . '/app/Http/Controllers/ClientsController.php',
     'App\\Http\\Controllers\\Controller' => $baseDir . '/app/Http/Controllers/Controller.php',
     'App\\Http\\Controllers\\LivreController' => $baseDir . '/app/Http/Controllers/LivreController.php',
     'App\\Http\\Controllers\\indexController' => $baseDir . '/app/Http/Controllers/indexController.php',
